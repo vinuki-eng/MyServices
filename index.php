fda135f38b6704aa8d1d4d10da229e291d366925
Vinuki <neththikumaravinuki@gmail.com>
2025-11-07 20:53:51 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <title>
+           MyServices
+        </title>
+    </head>
+    <body>
+        <div class="navbar-div">
+            <nav class="nav-bar">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About-me</a></li>
+                    <li><a href="">Services</a></li>
+                    <li><a href=""> Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </body>
+</html>
